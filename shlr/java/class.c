33e29f5c1bbb1671b8891551d70fb986bfe45370
pancake <pancake@nopcode.org>
2018-09-21 02:16:54 +0200

    Fix #11112 - Rename {srwx,flags,perms} to perm. (-21 LOC)
    Unify R_IO, R_BIN, R_BP, .. into R_PERM_* using 1 letter syntax

--- shlr/java/class.c
+++ shlr/java/class.c
@@ -2650,7 +2650,7 @@ R_API RList *r_bin_java_get_sections(RBinJavaObj *bin) {
 			strcpy (section->name, "constant_pool");
 			section->size = bin->cp_size;
 			section->paddr = bin->cp_offset + baddr;
-			section->srwx = R_BIN_SCN_READABLE;
+			section->perm = R_PERM_R;
 			section->add = true;
 			r_list_append (sections, section);
 		}
@@ -2662,7 +2662,7 @@ R_API RList *r_bin_java_get_sections(RBinJavaObj *bin) {
 			strcpy (section->name, "fields");
 			section->size = bin->fields_size;
 			section->paddr = bin->fields_offset + baddr;
-			section->srwx = R_BIN_SCN_READABLE;
+			section->perm = R_PERM_R;
 			section->add = true;
 			r_list_append (sections, section);
 			section = NULL;
@@ -2675,7 +2675,7 @@ R_API RList *r_bin_java_get_sections(RBinJavaObj *bin) {
 					snprintf (section->name, R_BIN_SIZEOF_STRINGS, "attrs.%s", fm_type->name);
 					section->size = fm_type->size - (fm_type->file_offset - fm_type->attr_offset);
 					section->paddr = fm_type->attr_offset + baddr;
-					section->srwx = R_BIN_SCN_READABLE;
+					section->perm = R_PERM_R;
 					section->add = true;
 					r_list_append (sections, section);
 				}
@@ -2688,7 +2688,7 @@ R_API RList *r_bin_java_get_sections(RBinJavaObj *bin) {
 			strcpy (section->name, "methods");
 			section->size = bin->methods_size;
 			section->paddr = bin->methods_offset + baddr;
-			section->srwx = R_BIN_SCN_READABLE | R_BIN_SCN_EXECUTABLE;
+			section->perm = R_PERM_RX;
 			section->add = true;
 			r_list_append (sections, section);
 			section = NULL;
@@ -2701,7 +2701,7 @@ R_API RList *r_bin_java_get_sections(RBinJavaObj *bin) {
 					snprintf (section->name, R_BIN_SIZEOF_STRINGS, "attrs.%s", fm_type->name);
 					section->size = fm_type->size - (fm_type->file_offset - fm_type->attr_offset);
 					section->paddr = fm_type->attr_offset + baddr;
-					section->srwx = R_BIN_SCN_READABLE;
+					section->perm = R_PERM_R;
 					section->add = true;
 					r_list_append (sections, section);
 				}
@@ -2714,7 +2714,7 @@ R_API RList *r_bin_java_get_sections(RBinJavaObj *bin) {
 			strcpy (section->name, "interfaces");
 			section->size = bin->interfaces_size;
 			section->paddr = bin->interfaces_offset + baddr;
-			section->srwx = R_BIN_SCN_READABLE;
+			section->perm = R_PERM_R;
 			section->add = true;
 			r_list_append (sections, section);
 		}
@@ -2726,7 +2726,7 @@ R_API RList *r_bin_java_get_sections(RBinJavaObj *bin) {
 			strcpy (section->name, "attributes");
 			section->size = bin->attrs_size;
 			section->paddr = bin->attrs_offset + baddr;
-			section->srwx = R_BIN_SCN_READABLE;
+			section->perm = R_PERM_R;
 			section->add = true;
 			r_list_append (sections, section);
 		}
